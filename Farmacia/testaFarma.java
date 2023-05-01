e6d7015dcb6884087d3bed955264bc0aba9ccb8d
camilameeira <meiracamila91@gmail.com>
2023-04-30 21:48:12 -0300

    Primeiro commit

---
+++ Farmacia/testaFarma.java
@@ -0,0 +1,15 @@
+package Farmacia;
+
+import java.util.Scanner;
+
+public class testaFarma {
+
+	public static void main(String[] args) {
+
+		Remedio remd1 = new Remedio(1, "Dove", 500f, "Alodipino", 10, "verde", 3);
+
+		remd1.visualizar();
+
+	}
+
+}
